9c3f87853fc0ded074d02441ba088f0c88e17e9b
dzakybrori <broridzaky2702@gmail.com>
2021-11-30 01:43:51 +0700

    Adding activelink on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,24 @@ var swiper = new Swiper(".portofolio__container", {
     },
   });
 
-/*======================= QUALIFICATION TABS ======================*/
+/*================== SCROLL SECTION ACTIVE LINK =================*/
+const sections = document.querySelectorAll('section[id]')
 
-/*======================= SERVICE MODAL ======================*/
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*======================== SERVICE MODAL ========================*/
