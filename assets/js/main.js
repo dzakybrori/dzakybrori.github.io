ac448f9ae73ddf8a483901de9963845533c3f117
dzakybrori <broridzaky2702@gmail.com>
2021-11-30 01:51:04 +0700

    Adding header shadow on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,4 +62,10 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
-/*======================== SERVICE MODAL ========================*/
+/*=================== CHANGE BACKGROUND HEADER ===================*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
